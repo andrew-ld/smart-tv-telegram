a4c39c63dd076a5a91990fb89f73e1011bbddccd
andrew (from workstation) <andrew@arch>
2020-07-22 17:03:11 +0200

    config parser errors on argument parser

--- smart_tv_telegram/__main__.py
+++ smart_tv_telegram/__main__.py
@@ -1,4 +1,5 @@
 import asyncio
+import configparser
 import logging
 import argparse
 import os.path
@@ -6,32 +7,42 @@ import os.path
 from smart_tv_telegram import *
 
 
-def is_valid_file(parser, arg):
+def open_config(parser: argparse.ArgumentParser, arg: str) -> Config:
     if not os.path.exists(arg):
-        parser.error(f"The file {arg} does not exist!")
+        parser.error(f"The file `{arg}` does not exist")
 
-    return arg
+    elif not os.path.isfile(arg):
+        parser.error(f"`{arg}` is not a file")
+
+    try:
+        return Config(arg)
+    except ValueError as err:
+        parser.error(str(err))
+    except KeyError as err:
+        parser.error(f"config key {str(err)} does not exists")
+    except configparser.Error as err:
+        parser.error(f"generic configparser error:\n{str(err)}")
 
 
-async def async_main(config_path: str):
-    config = Config(config_path)
+async def async_main(config: Config):
     mtproto = Mtproto(config)
     http = Http(mtproto, config)
-    bot = Bot(mtproto, config)
 
+    bot = Bot(mtproto, config)
     bot.prepare()
+
     await mtproto.start()
     await http.start()
 
 
-def main(config_path: str):
+def main(config: Config):
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(async_main(config_path))
+    loop.run_until_complete(async_main(config))
 
 
 def arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", type=lambda x: is_valid_file(parser, x), default="config.ini")
+    parser.add_argument("-c", "--config", type=lambda x: open_config(parser, x), default="config.ini")
     parser.add_argument("-v", "--verbosity", type=int, choices=[0, 1, 2], default=0)
 
     args = parser.parse_args()
