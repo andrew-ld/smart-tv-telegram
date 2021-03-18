d924cf490d24968b3f6eb4881cb878e3075bd609
andrew (from workstation) <andrew-ld@protonmail.com>
2021-03-18 23:13:39 +0100

    new release: 3.7.1, bugfix chromecast

--- smart_tv_telegram/__init__.py
+++ smart_tv_telegram/__init__.py
@@ -4,8 +4,8 @@ from .http import Http, OnStreamClosed
 from .bot import Bot
 
 
-__version__ = "1.3.0"
-__version_info__ = ("1", "3", "0")
+__version__ = "1.3.1"
+__version_info__ = ("1", "3", "1")
 __author__ = "https://github.com/andrew-ld"
 
 
