e95a44432acc2ff9375c8bc5362738ac754f3b7e
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2024-01-07 20:30:31 +0100

    1.3.8

--- smart_tv_telegram/__init__.py
+++ smart_tv_telegram/__init__.py
@@ -4,8 +4,8 @@ from .devices_collection import DeviceFinderCollection
 from .http import Http, OnStreamClosed
 from .bot import Bot
 
-__version__ = "1.3.7"
-__version_info__ = ("1", "3", "7")
+__version__ = "1.3.8"
+__version_info__ = ("1", "3", "8")
 __author__ = "https://github.com/andrew-ld"
 
 __all__ = [
